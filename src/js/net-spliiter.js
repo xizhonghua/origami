632f9b0ad890bf264174cd5552cc8d2b4e80aecc
Zhonghua Xi <xizhonghua@gmail.com>
2015-04-03 21:22:46 -0400

    update

---
+++ src/js/net-spliiter.js
@@ -0,0 +1,10 @@
+$(document).ready(function($) {
+
+var origami = new Origami.Model();
+
+origami.load('models/star-024.json', function(model){
+    console.log('origami loaded!');
+});
+
+
+});
--- src/js/ori.js
+++ src/js/ori.js
@@ -1,15 +1,41 @@
-var Origami = function() {
+// requires:
+//  jQuery
+//  Three.js
 
-    // flat vertices
+// namespace
+var Origami = Origami || {};
+
+// crease class
+Origami.Crease = function() {
+
+}
+
+// face class
+Origami.Face = function(vid1, vid2, vid3, pid, cid) {
+
+    // verter indices
+    this.vids = [vid1, vid2, vid3];
+    
+    // parent face id
+    this.pid = pid;
+    
+    // crease id
+    this.cid = cid;
+}
+
+// model class
+Origami.Model = function() {
+
+    // flat vertices, array of THREE.vector3
     this.flat_vertices = [];
 
-    // folded vertices
+    // folded vertices, array of THREE.vector3
     this.vertices = [];
 
-    // faces, each face is ab array of 3 vertices id
+    // faces, array of Origami.Face
     this.faces = [];
 
-    //  
+    //  creases, array of Origami.Crease
     this.creases = [];
 
     // array of array of folding angles
@@ -18,21 +44,71 @@ var Origami = function() {
     // the parent face ids
     this.parent_face_ids = [];
 
+    // base face id
+    this.base_face_id = 0;
+
     // ordered face list
-    this.ordered_face_ids = [];
+    this.ordered_face_ids = []; 
+}
+
+Origami.Model.prototype.load = function(url, callback) {
+    var me = this;
+    $.getJSON(url, function(json, textStatus) {
+        me.build(json);
+        if(callback) callback(me);
+    });
 }
 
 // build the origami model from data in string
-Origami.prototype.build = function(data) {
+Origami.Model.prototype.build = function(model) {
     // body...
+    console.log('building origami model...');
+
+    // bace face
+    this.base_face_id = model.base_face_id;
+
+    // building vertices
+    for(var i=0;i<model.vertices.length;++i)
+    {
+        var v = model.vertices[i];
+
+        var vertex = new THREE.Vector3(v[0], v[1], v[2]);        
+
+        this.vertices.push(vertex);
+
+        // this.flat_vertices is a deep copy of this.vertices
+        this.flat_vertices.push(vertex.clone());
+    }
+
+    // building faces
+    for(var i=0;i<model.faces.length;++i)
+    {
+        var f = model.faces[i];
+
+        var face = new Origami.Face(f[1],f[2],f[3],f[4],f[5]);
+
+        this.faces.push(face);
+    }
+
+    //building creases
+    for(var i=0;i<models.creases.length;++i)
+    {
+        var c = model.creases[i];
+
+        var crease = new Origami.Crease();
+
+        this.creases.push(crease);
+    }
+
+    console.log('built!');
 };
 
 // add folding path data
-Origami.prototype.addFoldingPath = function(path) {    
+Origami.Model.prototype.addFoldingPath = function(path) {    
     this.folding_path = path;
 };
 
 // fold the origami to certen percentage
-Origami.prototype.foldTo = function(percentage) {
+Origami.Model.prototype.foldTo = function(percentage) {
     // body...
 };
