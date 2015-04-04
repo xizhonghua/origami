d26b00a015f1b0e6a0e1d30d66bf821b48f0ad34
Zhonghua Xi <xizhonghua@gmail.com>
2015-04-03 20:35:39 -0400

    implmented exporting *.ori to json

--- tools/ori_convert.py
+++ tools/ori_convert.py
@@ -4,12 +4,74 @@
 
 import sys
 import os
+import json
 
 
 def convert_ori_to_json(input, output):
+
+    print 'reading form', input
+    print 'exporting to', output
+
+    fin = open(input, 'r')
+    fout = open(output, 'w')
+
+    # read vsize
+    vsize = int(fin.readline())
+
+    ori = {}
+    vertices = []
+    faces = []
+
+    for i in range(0, vsize):
+        coord = [float(x) for x in fin.readline().split()]
+        vertices.append(coord)
+
+    ori['vertices'] = vertices
+
+    # read csize
+
+    csize = int(fin.readline())
+    creases = []
+    for i in range(0, csize):
+        items = fin.readline().split()
+        crease = [int(items[0]), int(items[1]), float(items[2]), int(items[3]), int(items[4])]
+        creases.append(crease)
+
+    ori['creases'] = creases
+
+    # read faces
+
+    fsize = int(fin.readline())
+    faces = []
+    for i in range(0, fsize):
+        face = [int(x) for x in fin.readline().split()]
+        faces.append(face)
+
+    ori['faces'] = faces
+
+    # base face
+    base_face_id = int(fin.readline())
+    ordered_faces = []
+
+    # ordered face list
+    for i in range(0, fsize):
+        ordered_faces.append(int(fin.readline()))
+
+    ori['base_face_id'] = base_face_id
+    ori['ordered_faces'] = ordered_faces
+
+    # make it a little bit human readable
+    json_str = json.dumps(ori, indent=2)
+
+    fout.write(json_str)
+
+    fin.close()
+    fout.close()
+
     pass
 
 def convert_traj_to_json(input, output):
+    # TODO
     pass
 
 
