4114efb2d93936d269895580dddf68d8bf8586f5
xizhonghua <xizhonghua@gmail.com>
2015-05-13 00:41:48 -0400

    update

---
+++ src/js/inflate.js
@@ -0,0 +1,9 @@
+// inflate the current geometry
+function inflate(cur_g, org_g, p) {
+	//TODO
+}
+
+// measure the number of hyperbolic vertices in the mesh
+function measure_hyperbolic(g)  {
+	//TODO
+}
