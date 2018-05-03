24cf9429e81dbd56028b239fd0cf89ffb709efce
Zhonghua Xi <xizhonghua@gmail.com>
2018-05-02 22:11:03 -0700

    update

--- src/backend/upload.php
+++ src/backend/upload.php
@@ -1,13 +1,19 @@
 <?php
-$target_dir = "uploads/";
+$target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["model"]["name"]);
 
 echo $target_file . "<br/>";
 
+$method = $_POST["method"];
+$iterations = $_POST["iterations"];
+
+echo "Method: " . $method . " iterations: " . $iterations; 
+
 if (move_uploaded_file($_FILES["model"]["tmp_name"], $target_file)) {
+  // $cmd_method 
   $cmd = "bin/unfolder -g " . $target_file . " > uploads/log.txt &";
   echo $cmd . "<br/>";
-  exec($cmd);
+  // exec($cmd);
 } else {
   echo "Sorry, there was an error uploading your file.";
 }
