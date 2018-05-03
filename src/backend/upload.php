8b4807d553069c5547ebad446c36816f60f982a9
Zhonghua Xi <xizhonghua@gmail.com>
2018-05-02 23:07:21 -0700

    update

--- src/backend/upload.php
+++ src/backend/upload.php
@@ -6,14 +6,17 @@ echo $target_file . "<br/>";
 
 $method = $_POST["method"];
 $iterations = $_POST["iterations"];
-
-echo "Method: " . $method . " iterations: " . $iterations; 
+$max_generations = $_POST["max-generations"];
+$args = "-q -g " . " -r " . $iterations . " -h " . $method . " ";
+if ($method == "ga") {
+  $args = $args . " -mg " . $max_generations . " -ga bin/unfolding.ga ";
+}
 
 if (move_uploaded_file($_FILES["model"]["tmp_name"], $target_file)) {
   // $cmd_method 
-  $cmd = "bin/unfolder -g " . $target_file . " > uploads/log.txt &";
+  $cmd = "bin/unfolder " . $args . $target_file . " > ../uploads/log.txt &";
   echo $cmd . "<br/>";
-  // exec($cmd);
+  exec($cmd);
 } else {
   echo "Sorry, there was an error uploading your file.";
 }
